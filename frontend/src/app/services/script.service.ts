ecb1df898fa1ca09706bf87b7f4afdb2fd6c31d8
Eyajrah <eyajrah6@gmail.com>
2025-05-15 11:47:14 +0100

    fix: utilisation de l'URL de l'API depuis l'environnement dans le service de scan

--- frontend/src/app/services/script.service.ts
+++ frontend/src/app/services/script.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ScriptService {
+  private readonly backendUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
   runScript(repoUrl: string): Observable<any> {
-    return this.http.get<any>('http://localhost:5000/api/run-script', { params: { repoUrl } });
+    return this.http.get<any>(`${this.backendUrl}/run-script`, { params: { repoUrl } });
   }
 
   runScanAndSend(repoUrl: string): Observable<any> {
-    return this.http.get<any>('http://localhost:5000/api/run-script', { params: { repoUrl } });
+    return this.http.get<any>(`${this.backendUrl}/run-script`, { params: { repoUrl } });
   }
 } 
