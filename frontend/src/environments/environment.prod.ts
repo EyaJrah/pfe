742c5ad4127829050bfed78ca06f35f1899cd6ba
Eyajrah <eyajrah6@gmail.com>
2025-05-15 11:31:59 +0100

    feat: ajout de la configuration de production pour l'API

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,29 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  },
+  apiUrl: 'https://pfe-app-imrs.onrender.com/api',
+  securityTools: {
+    sonarqube: {
+      apiUrl: 'https://sonarcloud.io/api',
+      token: '298d7357212881052db75cc289eb19e14284d9ce'
+    },
+    trivy: {
+      apiUrl: 'https://pfe-app-imrs.onrender.com/api/trivy'
+    },
+    snyk: {
+      apiUrl: 'https://snyk.io/api/v1',
+      token: '30901ddf-2cbe-45a3-b12b-c8f5c9b1115d',
+      orgId: 'EyaJrah'
+    },
+    owasp: {
+      apiUrl: 'https://pfe-app-imrs.onrender.com/api/owasp'
+    }
+  }
+}; 
